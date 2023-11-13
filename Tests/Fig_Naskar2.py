f1ebdf756a0f3a97a50d32a276b770e94b8c4ba1
FaragoFa <facundofarago@gmail.com>
2023-11-13 09:44:47 -0300

    Se agrego Naskar. Anda meh

---
+++ Tests/Fig_Naskar2.py
@@ -0,0 +1,72 @@
+# ================================================================================================================
+#
+# This prog. plots the max frec for varying global couplings (G)
+#
+# see:
+# [D*2014]  Deco et al. (2014) J Neurosci.
+#           http://www.jneurosci.org/content/34/23/7886.long
+#
+# By Gustavo Patow
+#
+# Optimized by Facundo Faragó
+# ================================================================================================================
+
+# ============== import libraries
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+
+# ============== chose a model
+import Models.Naskar as Naskar
+
+# ============== chose and setup an integrator
+import WholeBrain.Integrators.EulerMaruyama as integrator
+integrator.neuronalModel = Naskar
+integrator.verbose = False
+
+# ============== chose a FIC mechanism
+import WholeBrain.Utils.FIC.BalanceFIC as BalanceFIC
+BalanceFIC.integrator = integrator
+import WholeBrain.Utils.FIC.Balance_Herzog2022 as Herzog2022Mechanism
+BalanceFIC.balancingMechanism = Herzog2022Mechanism  # default behaviour for this project
+
+
+np.random.seed(42)  # Fix the seed for debug purposes...
+def plotMaxFrecForAllWe(C, wStart=0, wEnd=6+0.001, wStep=0.05,
+                        extraTitle='', precompute=True, fileName=None):
+    # Integration parms...
+    dt = 0.1
+    tmax = 10000.
+    Tmaxneuronal = int((tmax+dt))
+    # all tested global couplings (G in the paper):
+    wes = np.arange(wStart + wStep, wEnd, wStep)  # warning: the range of wes depends on the conectome.
+    N = C.shape[0]
+    Naskar.setParms({'SC': C})
+    print("======================================")
+    print("=    simulating FIC                  =")
+    print("======================================")
+    # DMF.lambda = 0.  # make sure no long-range feedforward inhibition (FFI) is computed
+    maxRateFIC = np.zeros(len(wes))
+    for kk, we in enumerate(wes):  # iterate over the weight range (G in the paper, we here)
+        print("\nProcessing: {}  ".format(we), end='')
+        Naskar.setParms({'we': we})
+        integrator.recompileSignatures()
+        v = integrator.simulate(dt, Tmaxneuronal)[:,1,:]  # [1] is the output from the excitatory pool, in Hz.
+        maxRateFIC[kk] = np.max(np.mean(v,0))
+        print("maxRateFIC => {}".format(maxRateFIC[kk]))
+    fic, = plt.plot(wes, maxRateFIC)
+    fic.set_label("FIC")
+
+    for line, color in zip([1.47, 4.45], ['r','b']):
+        plt.axvline(x=line, label='line at x = {}'.format(line), c=color)
+    plt.title("Large-scale network (DMF)" + extraTitle)
+    plt.ylabel("Maximum rate (Hz)")
+    plt.xlabel("Global Coupling (G = we)")
+    plt.legend()
+    plt.show()
+
+
+
+# ==========================================================================
+# ==========================================================================
+# ========================================================================== --EOF
